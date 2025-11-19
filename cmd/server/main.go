f1359c962972cab840965f4a2b1f4f9b0bcd01c3
seiobata <s.bautista18@yahoo.com>
2025-11-19 10:00:16 -0600

    create server channel and publish JSON function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/seiobata/peril/internal/pubsub"
+	"github.com/seiobata/peril/internal/routing"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -15,10 +18,27 @@ func main() {
 	fmt.Println("Starting Peril server...")
 	conn, err := amqp.Dial(connection_string)
 	if err != nil {
-		log.Fatalf("problem connecting to Peril server: %v", err)
+		log.Fatalf("problem connecting to RabbitMQ: %v", err)
 	}
 	defer conn.Close()
-	fmt.Println("Connected to Peril server.")
+	fmt.Println("Peril server connected to RabbitMQ!")
+
+	pubChan, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("channel could not be opened: %v", err)
+	}
+
+	err = pubsub.PublishJSON(
+		pubChan,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Printf("problem publishing message: %v", err)
+	}
 
 	// waiting for signal
 	sigChan := make(chan os.Signal, 1)
